8992cb396c3e4f2535f0923a128013d3b7021fda
Carlos Alberto Fiallos Bejarano <carlosfiallos@hotmail.es>
2022-05-17 11:19:44 -0500

    padding al error

--- src/components/Error/error.js
+++ src/components/Error/error.js
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <section className="page_404">
+    <section className="page_404" style={{
+			padding: "19%",
+		}} >
       <div className="container">
         <div className="row">
           <div className="col-sm-12 ">
