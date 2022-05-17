b8a673c0e404f3fc164afb75b08b8fc211ac610f
Carlos Alberto Fiallos Bejarano <carlosfiallos@hotmail.es>
2022-05-17 17:26:10 -0500

    Correción de los primeros parrafos del landing

--- src/components/utils/landing.js
+++ src/components/utils/landing.js
@@ -1,18 +1,18 @@
 export const landing = {
-    block_1: {
-        image: '/img/pafta.png',
-        altImg: 'Pafta',
-        title: 'Plataforma Informática de Alertas',
-        text: 'El presente proyecto de investigación tiene como objetivo mejorar el proceso productivo y prevención de plagas, mediante la implementación de una plataforma informática de alertas tempranas fitosanitarias agrícolas enfocadas en primera instancia a una de las enfermedades que más afecta a este cultivo como es el caso de la Monilla (Moniliophthora roreri). El proyecto cuenta con el trabajo en conjunto entre docentes investigadores, especialistas, pasantes y estudiantes de la ESPOCH, UNACH, MAG, INIAP así también, junto con los productores del cantón.',
-        text1:'La plataforma estará constituida por cuatro componentes: red de estaciones meteorológicas, modelos predictivos, sistemas de información geográfica – teledetección y un portal web que funcionarán articuladamente para procesar los datos y presentar la información que estará a disposición de investigadores, productores, instituciones gubernamentales y demás interesados. Adicionalmente, permitirá alertar a los productores sobre posibles riesgos de plagas o enfermedades, así como la provisión de información (manuales, mapas temáticos, entre otros) para prevenir o remediar estas con procedimientos actuales y en base a estándares y buenas prácticas.'
-    },
-    block_2: {
-        image: '/img/pafta2.png',
-        altImg: 'PaftaMegadata',
-        title: 'Megadata de la Plataforma Informática de Alertas',
-        text: ''
-    }
-    /*block_3: {
+	block_1: {
+		image: "/img/pafta.png",
+		altImg: "Pafta",
+		title: "Plataforma Informática de Alertas",
+		text: "Este proyecto tiene por objetivo mejorar la productividad y prevención de plagas mediante la implementación de una plataforma informática de alertas tempranas fitosanitarias, enfocadas inicialmente en la monilla (monoliophtora roreri), una de las plagas más frecuentes en las plantaciones de cacao; desarrollado como un trabajo articulado entre docentes investigadores, especialistas y estudiantes de la Escuela Superior Politécnica de Chimborazo, Universidad Nacional de Chimborazo, Ministerio de Agricultura y Ganadería, Instituto Nacional de Investigaciones Agropecuarias de Ecuador y productores locales.",
+		text1: "La plataforma estará constituida por cuatro componentes: red de estaciones meteorológicas, modelos predictivos, sistemas de información geográfica – teledetección y un portal web que funcionarán articuladamente para procesar los datos y presentar la información que estará a disposición de investigadores, productores, instituciones gubernamentales y demás interesados. Adicionalmente, permitirá alertar a los productores sobre posibles riesgos de plagas o enfermedades, así como la provisión de información (manuales, mapas temáticos, entre otros) para prevenir o remediar estas con procedimientos actuales y en base a estándares y buenas prácticas.",
+	},
+	block_2: {
+		image: "/img/pafta2.png",
+		altImg: "PaftaMegadata",
+		title: "Megadata de la Plataforma Informática de Alertas",
+		text: "",
+	},
+	/*block_3: {
         image: '/src/img/estudio.svg',
         altImg: 'retroalimentacion real y personalizada',
         title: 'Recibe retroalimentación real',
@@ -24,7 +24,7 @@ export const landing = {
         title: 'Fundamentos del proyecto',
         text: '4Skillsweb es un proyecto de investigación  el cual busca aplicar una metodologia educativa para mejorar la destreza en el aprendizaje del idioma inglés basada en el diseño e implementación de la aplicación 4skillsweb en los estudiantes del nivel B1 de la facultad de informática y electrónica de la Escuela Superior Politécnica de Chimborazo 2020'
     }*/
-}
+};
 
 /*export const features = [
     {
